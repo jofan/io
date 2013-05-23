00aa400f8e7b38a9e18754d3fe2fbff4c530377e
jofan <stefan.liden@gmail.com>
2013-05-23 23:23:39 +0200

    Changed ports config

--- server.js
+++ server.js
@@ -10,11 +10,14 @@ var express = require("express"),
 
 // Configure
 
+var port = 3333;
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.engine('.html', cons.swig);
 app.set('view engine', 'html');
 // app.set("view options", {layout: false});
+app.set("port", process.env.PORT || port);
 app.use(express.bodyParser());
 app.use(express.static(__dirname + '/public'));
 
@@ -31,5 +34,5 @@ app.get('/', function (req, res) {
     res.render('index.html', { foo: 'bar' });
 });
 
-app.listen(3333);
-console.log('listening on 3333');
+app.listen(app.settings.port);
+console.log('listening on ' + app.settings.port);
